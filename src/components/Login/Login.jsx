99b28d92bb6b972915ce09c4d5ed91acd7f6c15e
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-04 02:28:13 +0600

    intregate google sign in method

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
-
+import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 const Login = () => {
-
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn =() =>{
+    signInWithPopup(auth,provider)
+    .then(res=>{
+      const user = res.user
+      console.log(user)
+    })
+    .catch(error=>{
+      console.log('error: ',error.message)
+    })
+  } 
   return (
-    <div className='h-screen'>
-      login
+    <div className="h-screen m-5">
+      <div className="flex justify-end">
+        <div className="border border-pink-900 w-80   mt-10 rounded-xl py-10">
+          <div className="flex items-center justify-center gap-x-2 my-1">
+            <label className="" htmlFor="">
+              User Name
+            </label>
+            <input
+              className="border border-pink-900 p-1 rounded-lg"
+              type="text"
+            />
+          </div>
+          <div className="flex items-center justify-center gap-x-2 my-2">
+            <label className="" htmlFor="">
+              Password
+            </label>
+            <input
+              className="border border-pink-900 p-1 rounded-lg"
+              type="text"
+            />
+          </div>
+          <div className="flex items-center justify-end gap-x-2 my-2">
+            <button className="bg-pink-900 text-pink-100 px-4 py-1 rounded-lg">
+              Login
+            </button>
+          </div>
+          <div className="flex items-center justify-center gap-x-2 my-2">
+            <a onClick={handleGoogleSignIn} className="border cursor-pointer border-pink-900 text-pink-900 px-4 py-1 rounded-lg">
+              Login With Google
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
