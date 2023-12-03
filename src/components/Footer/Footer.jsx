2d566dec2966d859bd2ed92713bebffd6f4c4a18
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-03 17:21:27 +0600

    change footer developed by text color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='bg-pink-900  py-4 text-center text-sm font-bold text-pink-100'>
-      <p className="">© All right reserve</p>
-      <p className="">Developed by <a href='https://github.com/ab-siddiq' target='_blank'>Md. Abu Bakkar Siddiq</a></p>
+    <div className="bg-pink-900  py-4 text-center text-sm font-bold text-pink-100">
+      <p className="">© All right reserved.</p>
+      <p className="">
+        Developed by{" "}
+        <a
+          className="text-pink-400"
+          href="https://github.com/ab-siddiq"
+          target="_blank"
+        >
+          Md. Abu Bakkar Siddiq
+        </a>
+      </p>
     </div>
   );
 };
